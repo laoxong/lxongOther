7ef7f47412b0b66232afb4e44a1ae24eb1805c2d
laoxong <i@moec.top>
2020-03-19 17:50:47 +0800

    caddy防CC

--- Bash/caddy-anit-cc.sh
+++ Bash/caddy-anit-cc.sh
@@ -1,12 +1,13 @@
  
 #/bin/bash
 #Gayhub:https://github.com/lixworth/CFBlockIP
+
 #日志文件，你需要改成你自己的路径
 
 logfile=/usr/local/caddy/log/
 #结束时间现在
 
-end_time=date +%s
+end_time=`date +%s`
 #echo $end_time
 
 #开始时间1分钟之前
@@ -18,21 +19,33 @@ start_time=$(( end_time - 60 ))
 
 tac $logfile/web.log | awk -v st="$start_time" -v et="$end_time" '{if(($8 > st || $8 == st) && ($8 < et || $8 == et)) {print $3}}' | sort | uniq -c | sort -nr > $logfile/log_ip_top
 
-ip_top=cat $logfile/log_ip_top | head -1 | awk '{print $1}'
+ip_top=`cat $logfile/log_ip_top | head -1 | awk '{print $1}'`
 
-#单位时间[1分钟]内相同ip访问次数超过 60 次自动加入到 Cloudflare 防火墙.
+#单位时间[1分钟]内相同ip访问次数超过 n 次自动加入到 Cloudflare 防火墙. （这里 5 次是做测试用的。）
 
-ip=`cat $logfile/log_ip_top | awk '{if($1>60) print $2}'
+ip=`cat $logfile/log_ip_top | awk '{if($1>5) print $2}'
+
 # 填 Cloudflare 帐号的 Email 邮箱
 CFEmail=""
+
 # 填 Cloudflare 帐号的 Global API Key
+
 GlobalAPIKey=""
+
 # 填 Cloudflare 域名对应的 Zone ID
+
 ZoneID=""
+
 for IPAddr in $ip; do
+if[$IPAddr -eq "127.0.0.1"]; then
+
+else
+
 curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$ZoneID/firewall/access_rules/rules" \
   -H "X-Auth-Email: $CFEmail" \
   -H "X-Auth-Key: $GlobalAPIKey" \
   -H "Content-Type: application/json" \
-  --data '{"mode":"block","configuration":{"target":"ip","value":"'$IPAddr'"},"notes":"恶意攻击"}'
+  --data '{"mode":"block","configuration":{"target":"ip","value":"'$IPAddr'"},"notes":"CC/DDOS Attatch"}'
+
+fi
 done

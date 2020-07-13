2e0961eb8aafe2fbbe9f55319afd883a3e0961b1
laoxong <lao__xong@outlook.com>
2020-07-13 10:48:34 +0800

    FTP备份

---
+++ Bash/ftpbackupsql.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+BAK_DIR="/data/backup/" #本机备份文件临时存储目录
+WEB_DIR="/data/web/*" #要备份的网站文件
+FTP_USER="username" #FTP用户名
+FTP_PWD="password" #FTP密码
+DB="XXX" #要备份的数据库
+DB_PWD="XXXXXX" #数据库root 用户密码
+DATE_TIME=$(date +%F-%T)
+DATE=$(date +%Y%m%d)
+MYLOG_DIR='/var/log/mylogs'
+mkdir -p /var/log/mylogs
+DATE_TIME=$(date +%F-%T)
+if [ -d BAK_DIR ]; then
+chmod -R 755 $BAK_DIR
+else
+mkdir -p $BAK_DIR
+chmod -R 755 $BAK_DIR
+fi
+#web backup
+tar -zcPf $BAK_DIR/web_$DATE.tar.gz $WEB_DIR 2>&1
+re1=$?
+if [[ $re1 == 0 ]]; then
+/bin/sync
+/bin/sync
+sleep 5
+echo "Data has been completed package! $DATE_TIME" >> $MYLOG_DIR/databak.log
+
+else
+echo "Web completed package Fail!" >> $MYLOG_DIR/databak.log
+fi
+#database backup
+mysqldump -u root -p$DB_PWD $DB > $BAK_DIR/db_$DATE.sql
+re2=$?
+if [[ $re2 == 0 ]]; then
+echo "Database Export Completed! $DATE_TIME" >> $MYLOG_DIR/databak.log
+else
+echo "Database Export Fail!" >> $MYLOG_DIR/databak.log
+fi
+if [[ $re1 == 0 ]] && [[ $re2 == 0 ]]; then
+echo "Began to upload file $DATE_TIME" >> $MYLOG_DIR/databak.log
+ftp -n<<!
+open 42.51.152.254
+user $FTP_USER $FTP_PWD
+binary
+hash
+cd /
+lcd $BAK_DIR
+prompt
+mput *
+close
+bye
+!
+echo "Upload successful! $DATE_TIME" >> $MYLOG_DIR/databak.log
+fi
+rm -rf $BAK_DIR/*
---
+++ Bash/ftpbackwithoutupsql.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+BAK_DIR="/data/backup/" #本机备份文件临时存储目录
+WEB_DIR="/data/web/*" #要备份的网站文件
+FTP_USER="username" #FTP用户名
+FTP_PWD="password" #FTP密码
+DATE_TIME=$(date +%F-%T)
+DATE=$(date +%Y%m%d)
+MYLOG_DIR='/var/log/mylogs'
+mkdir -p /var/log/mylogs
+DATE_TIME=$(date +%F-%T)
+if [ -d BAK_DIR ]; then
+chmod -R 755 $BAK_DIR
+else
+mkdir -p $BAK_DIR
+chmod -R 755 $BAK_DIR
+fi
+#web backup
+tar -zcPf $BAK_DIR/web_$DATE.tar.gz $WEB_DIR 2>&1
+re1=$?
+if [[ $re1 == 0 ]]; then
+/bin/sync
+/bin/sync
+sleep 5
+echo "Data has been completed package! $DATE_TIME" >> $MYLOG_DIR/databak.log
+
+else
+echo "Web completed package Fail!" >> $MYLOG_DIR/databak.log
+fi
+#Upload
+if [[ $re1 == 0 ]] && [[ $re2 == 0 ]]; then
+echo "Began to upload file $DATE_TIME" >> $MYLOG_DIR/databak.log
+ftp -n<<!
+open 42.51.152.254
+user $FTP_USER $FTP_PWD
+binary
+hash
+cd /
+lcd $BAK_DIR
+prompt
+mput *
+close
+bye
+!
+echo "Upload successful! $DATE_TIME" >> $MYLOG_DIR/databak.log
+fi
+rm -rf $BAK_DIR/*

a61f2591bb3f15915f6de5020c08bd1e871c5e54
laoxong <lao__xong@outlook.com>
2020-08-23 15:21:38 +0800

    宝塔

---
+++ Bash/bt.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "本脚本由老兄制作,仅供研究使用/n我的博客:https://www.moec.top"
+echo "------------------------"
+echo "1.开始运行"
+echo "2.添加hosts文件保护"
+echo "3.取消hosts文件保护"
+echo "4.退出"
+echo "------------------------"
+
+echo "请输入命令:"
+read choice
+case $choice in
+	1)
+	mkdir /www/server/panel/vhost/ssl/bt.cn -p
+	mkdir /www/wwwroot/www.bt.cn -p
+	curl https://raw.githubusercontent.com/laoxong/lxongOther/master/bt/bt.php -o /www/wwwroot/www.bt.cn/bt.php
+	curl https://raw.githubusercontent.com/laoxong/lxongOther/master/bt/www.bt.cn.conf -o /www/server/panel/vhost/nginx/www.bt.cn.conf
+	curl https://raw.githubusercontent.com/laoxong/lxongOther/master/bt/ssl/privkey.pem -o /www/server/panel/vhost/ssl/bt.cn/privkey.pem
+	curl https://raw.githubusercontent.com/laoxong/lxongOther/master/bt/ssl/fullchain.pem -o /www/server/panel/vhost/ssl/bt.cn/fullchain.pem
+	chown www:www /www/wwwroot/www.bt.cn/* -R
+	/etc/init.d/nginx reload
+	echo "127.0.0.1 www.bt.cn" >> /etc/hosts
+	chattr +i /etc/hosts
+	echo "完成!"
+	echo "请手动更新下软件列表"
+	;;
+
+	2)
+	chattr +i /etc/hosts
+	echo "已经添加hosts文件保护"
+	echo "如果您要修改hosts文件,请先取消保护"
+	;;
+
+	3)
+	chattr -i /etc/hosts
+	echo "hosts文件保护已经取消"
+	;;
+
+	4)
+	break
+	;;
+
+	*)
+	echo "你什么都不输入你到底想让我干什么..."
+	;;
+esac 
